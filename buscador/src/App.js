fde2b2c6f1e4f9fa0863ffb5b4e0d95d0174170e
Santiago Barreto <115950745+SanX0000@users.noreply.github.com>
2022-12-20 01:37:31 -0300

    finalizado CSS e função de busca antes da API

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -1,7 +1,18 @@
+import {useState} from 'react';
 import { FiSearch } from "react-icons/fi";
-import './styles,css';
+import './styles.css';
+
 
 function App() {
+
+  const [input, setInput] = useState ('')
+
+
+
+  function handleSearch(){
+    alert("VALOR DO INPUT " + input)
+  }
+
   return (
     <div className="container">
      <h1 className="title">Buscador CEP</h1>
@@ -9,9 +20,13 @@ function App() {
       <div className="containerInput">
         <input 
         type="text" 
-        placeholder="Digite seu CEP..."/>
+        placeholder="Digite seu CEP..."
+        value={input}
+        onChange={(e) => setInput(e.target.value) }
+        />
+        
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="FFF"/>
         </button>
       </div>
@@ -19,12 +34,11 @@ function App() {
         <main className="main">
           <h2>CEP: 79003222</h2>
 
-          <span>Rua Teste algum </span>
-          <span>Complemento: Algum complemento </span>
-          <span>Vila Rosa </span>
-          <span>Campo Grande - MS </span>
+          <span>Rua Teste algum</span>
+          <span>Complemento: Algum complemento</span>
+          <span>Vila Rosa</span>
+          <span>Campo Grande - MS</span>
           
-
         </main>
 
     </div>
